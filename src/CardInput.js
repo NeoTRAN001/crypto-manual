4096d7882fd4f934d45011e14ad0af8524089a38
NeoTRAN001 <neo_tran@outlook.com>
2020-09-07 19:13:35 -0500

    WebComponents

---
+++ src/CardInput.js
@@ -0,0 +1,61 @@
+import { LitElement, html, css } from 'lit-element';
+
+export class CardInput extends LitElement {
+
+    createRenderRoot() {
+        return this;
+    }
+
+    constructor() {
+        super();
+        this.encryption = 'A1Z26';
+        this.more = false;
+    }
+    
+    static get properties() {
+        return {
+            encryption: { type: String },
+            more: { type: Boolean }
+        }
+    }
+
+    static get styles() {
+        return css`
+            :host {
+                font-family: 'Josefin Sans';
+            }
+        `;
+    }
+
+    render() {
+        return html`
+            <div class="card">
+                <h4 class="card-header"> <i class="fa fa-shield-alt"></i> Encryption: ${ this.encryption }</h4>
+                <div class="card-body">
+                    <h4 class="card-title"><i class="fas fa-keyboard"></i> User Text</h4>
+                    
+                    ${this.more 
+                        ? html`` : html``
+                     }
+
+                    <div class="form-group">
+                        <textarea rows="6" class="form-control" placeholder="Your text"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <button type="button" class="btn btn-info">
+                        <i class="fas fa-eye-slash"></i> Cipher
+                        </button>
+                        <button type="button" class="btn btn-dark">
+                            <i class="fas fa-eye"></i> Decipher
+                        </button>
+                        <button type="button" class="btn btn-warning">
+                            <i class="fas fa-trash-alt"></i> Clear
+                        </button>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+}
+
+customElements.define('card-input', CardInput);
---
+++ src/HeaderComponent.js
@@ -0,0 +1,42 @@
+import { LitElement, html, css } from 'lit-element';
+
+export class HeaderComponent extends LitElement {
+
+    static get styles() {
+        return css`
+            :host {
+                width: 100%;
+            }
+
+            .header-text {
+                margin: 0;
+                font-family: 'Faster One', 'Josefin Sans', cursive;
+                animation-name: authorAnimation;
+                animation-duration: 3s;
+                animation-iteration-count: infinite;
+                animation-direction: alternate;
+            }
+
+            @keyframes authorAnimation {
+                0% {
+                    transform: scale(0.8);
+                }
+                100% {
+                    transform: scale(1.1);
+                }
+            }
+        `;
+    }
+
+    render() {
+        return html`
+            <center>
+                <h1 class="header-text">CryptoManual 1</h1>
+                <p  class="header-text">Team WASP</p>
+            </center>
+        `
+    }
+};
+
+
+customElements.define('header-component', HeaderComponent);
