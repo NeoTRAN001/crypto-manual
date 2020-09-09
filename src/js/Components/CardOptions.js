50d73da37aa7ec4f57101cb7ab9dc86957960042
NeoTRAN001 <neo_tran@outlook.com>
2020-09-08 22:39:16 -0500

    All option methods

--- src/js/Components/CardOptions.js
+++ src/js/Components/CardOptions.js
@@ -21,10 +21,17 @@ export class CardOptions extends LitElement {
                     <label class="input-group-text" for="inputGroupSelect01">Select</label>
                 </div>
                 <select class="custom-select" id="inputGroupSelect01">
-                    <option selected>A1Z26</option>
-                    <option value="1">Numerical Base</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
+                    <option value="0" selected>A1Z26</option>
+                    <option value="1">ROT13</option>
+                    <option value="2">Atbash</option>
+                    <option value="3">Escítala</option>
+                    <option value="4">Polybios</option>
+                    <option value="5">Repetition</option>
+                    <option value="6">Vigenère</option>
+                    <option value="7">Cesar</option>
+                    <option value="8">NumericalBase</option>
+                    <option value="9">Reverse</option>
+                    <option value="10">Emoji</option>
                 </select>
                 </div>
             </div>
