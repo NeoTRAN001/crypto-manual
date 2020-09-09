0903af315e10fca926633de9e73830233f3c26b5
NeoTRAN001 <neo_tran@outlook.com>
2020-09-08 23:40:20 -0500

    Show Method Name+

--- src/js/Components/CryptoInfo.js
+++ src/js/Components/CryptoInfo.js
@@ -6,8 +6,28 @@ export class CryptoInfo extends LitElement {
         return this;
     }
 
+    static get properties() {
+        return {
+            encryption: { type: String }
+        };
+    }
+
     render() {
         return html`
+            <div class="mt-4 alert alert-dismissible alert-primary">
+                Welcome to <strong>CryptoManual</strong>, a classic crypto website
+            </div>
+            <div class="container mt-4">
+                <div class="jumbotron jumbotron-fluid">
+                    <div class="container">
+                        <h1 class="display-4">${this.encryption}</h1>
+                        <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                    </div>
+                </div>
+                <div class="progress">
+                    <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
+                </div>
+            </div>
             <div class="container mt-4">
                 <div class="jumbotron jumbotron-fluid">
                     <div class="container">
@@ -30,18 +50,6 @@ export class CryptoInfo extends LitElement {
                     <div class="progress-bar progress-bar-striped bg-danger progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
                 </div>
             </div>
-            <div class="container mt-4">
-                <div class="jumbotron jumbotron-fluid">
-                    <div class="container">
-                        <h1 class="display-4">LitElement</h1>
-                        <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-                    </div>
-                </div>
-                <div class="progress">
-                    <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
-                </div>
-            </div>
-
         `;
     }
 }
