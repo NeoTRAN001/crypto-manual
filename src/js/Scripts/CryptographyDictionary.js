70a94006d0bbfdf5f9075e97fcf52d72790951e3
NeoTRAN001 <neo_tran@outlook.com>
2020-09-20 22:11:35 -0500

    Logic switch and methods

--- src/js/Scripts/CryptographyDictionary.js
+++ src/js/Scripts/CryptographyDictionary.js
@@ -1,5 +1,24 @@
-class CryptographyDictionary {
-    a1z26(message) {
-        console.log(message);
+import { CipherMessage } from './CipherMessage';
+import { DeCipherMessage } from './DecipherMessage';
+
+export class CryptographyDictionary {
+
+    main(message, encryption, inputs, action) {
+        const cipher = new CipherMessage();
+        const decipher = new DeCipherMessage();
+        let result = '';
+
+        switch(encryption) {
+            case 'A1Z26':
+                result = action 
+                    ? cipher.a1z26(message)
+                    : decipher.a1z26(message);
+                break;
+        }
+
+        return result;
     }
 }
+
+
+export default CryptographyDictionary;
